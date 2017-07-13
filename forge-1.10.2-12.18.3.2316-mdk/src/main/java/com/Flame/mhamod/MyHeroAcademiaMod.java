3b07eb1165d7178bcfd8b5a121800c178c2b12ff
FlameGamingHD <michaelhegarty@hotmail.co.uk>
2017-07-13 15:06:23 +0100

    The Modding Begins ;)

---
+++ forge-1.10.2-12.18.3.2316-mdk/src/main/java/com/Flame/mhamod/MyHeroAcademiaMod.java
@@ -0,0 +1,30 @@
+package com.Flame.mhamod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="MyHeroAcademiaMod", name="My Hero Academia Mod", version="1.10.2-1.0")
+public class MyHeroAcademiaMod
+{
+    @Mod.Instance("MyHeroAcademiaMod")
+    public static MyHeroAcademiaMod instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+    @Mod.EventHandler
+    public void Init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
